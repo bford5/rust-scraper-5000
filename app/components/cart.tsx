afddba84449a361853174cbaecf16246741187c9
bforddev7 <bforddev7@gmail.com>
2023-04-20 16:38:00 -0500

    continued work on cart-ui and displaying cart data

--- app/components/cart.tsx
+++ app/components/cart.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image"
 
+import formatPrice from "@/utils/PriceFormatter";
+
 import { useCartStore } from "@/Store/store"
 
 // NOTE: transition class is custom using tailwind utility classes, setup in global.css w/ @apply
@@ -10,11 +12,40 @@ export default function Cart() {
 
     const cartStore = useCartStore();
 
+    const CartItemsList = ({cart}) => {
+        
+        return (
+            <>
+            {cart.map((item)=> (
+                <div key={item.id} className=" flex flex-row justify-between bg-slate-300 rounded-2xl p-3">
+                    <div>
+                        {/* <Image src={item.images} alt={item.name} width={50} height={50} /> */}
+                        {/* item.images is being registered as null, am not able to find an image data being passed onto cart in local storage, need to review and fix this to have product image displayed in cart */}
+                    </div>
+                    <div className=" block text-left">
+                        <h2>{item.name}</h2>
+                        <p>{item.description}</p>
+                    </div>
+                    <div className=" block text-right">
+                        <h3>{item.unit_amount !== null ? formatPrice(item.unit_amount) : 'N/A'}</h3>
+                        <h5>Qt. <span className="bg-white rounded-full p-1">{item.quantity}</span></h5>
+                    </div>
+                </div>
+            ))}
+            </>
+        )
+    }
+
     return (
-        <>
-        <h2>CART MODAL</h2>
-            {/* <button className="p-2 bg-yellow-500 text-white rounded-md" onClick={toggleCart}>Cart</button> */}
+        <div onClick={() => {cartStore.toggleCart()}} className=" fixed w-full h-screen left-0 top-o bg-black/25">
+            {/* in order to stop cart closing when user interacts with cart modal below, add an onClick to stop propogation */}
+            <div onClick={(e) =>e.stopPropagation()} className="bg-white absolute right-0 top-0 w-1/4 h-screen py-12 px-8 overflow-y-scroll text-gray-700 flex flex-col gap-2">
+                <h2 className=" underline text-lg">CART MODAL</h2>
+                {cartStore.cart.length > 0 ? <CartItemsList cart={cartStore.cart} /> : 'No items in cart...'}
+                <button className=" w-full mx-2 rounded-xl text-lg bg-black text-white">Checkout</button>
+            </div>
+            {/* <button className="p-2 bg-yellow-500 text-white rounded-md" onClick={toggleCart}>Close</button> */}
             {/* {cartStore.isOpen && <CartModal />} */}
-        </>
+        </div>
     )
 }
--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -17,18 +17,22 @@ import { useCartStore } from "@/Store/store"
 export default function Navigation({user}: Session) {
     const cartStore = useCartStore();
     return (
-        <nav className=" mx-5 bg-slate-500 mt-1 rounded-md py-3 px-2 flex justify-between">
+        <nav className=" mx-5 bg-slate-500 mt-1 rounded-md px-2 flex justify-between gap-3">
             <div className="logo"><Link href={`/`} className="inline-block align-middle"><span className=" text-2xl"><DiRust /></span><span className=" text-lg">Buster</span></Link></div>
-            <div className="menu flex flex-row gap-1">
+            <div className="menu flex flex-row gap-1 py-3 ml-auto mr-0">
                 <Link className="p-2 hover:text-slate-800 hover:underline ease-in-out duration-700 transition-all" href={`/`}>Homepage</Link>
                 <Link className="p-2 hover:text-slate-800 hover:underline ease-in-out duration-700 transition-all" href={`/about`}>About</Link>
+            </div>
+            <div className="login py-3">
+                {!user ? <button onClick={() => signIn()} className="p-2 bg-red-400 text-white rounded-md transition">Login</button> : <button onClick={() => signOut()} className="p-2 bg-red-400 text-white rounded-md">Logout</button>}
+            </div>
+            <div className="cart my-auto">
                 <div onClick={cartStore.toggleCart} className="p-4 bg-teal-500 text-white rounded-md hover:bg-teal-700 hover:text-slate-300 transition text-xl cursor-pointer relative">
                     <FaShoppingCart />
                     {cartStore.cart.length >= 0 && <span className=" absolute text-sm bg-red-500 text-white rounded-full px-0.5 py-0.5 top-0 right-0 flex justify-center">{cartStore.cart.length}</span>}
                 </div>
-                {cartStore.isOpen && <Cart />}
-                {!user ? <button onClick={() => signIn()} className="p-2 bg-red-400 text-white rounded-md transition">Login</button> : <button onClick={() => signOut()} className="p-2 bg-red-400 text-white rounded-md">Logout</button>}
             </div>
+            {cartStore.isOpen && <Cart />}
         </nav>
     )
 }
